10b197dfc746b0833b1245eeda6c74dd5b82c3e0
JFLarvoire <jf.larvoire@free.fr>
2018-02-09 16:23:11 +0100

    Removed usused routines, and fixed -s and -x options.

--- x2s.c
+++ x2s.c
@@ -1,16 +1,14 @@
 /**
- * section: xmlWriter
- * synopsis: use various APIs for the xmlWriter
- * purpose: tests a number of APIs for the xmlWriter, especially
- *          the various methods to write to a filename, to a memory
- *          buffer, to a new document, or to a subtree. It shows how to
- *          do encoding string conversions too. The resulting
- *          documents are then serialized.
- * usage: testWriter
- * test: testWriter && for i in 1 2 3 4 ; do diff $(srcdir)/writer.xml writer$$i.tmp || break ; done
- * author: Alfred Mickautsch
- * copy: see Copyright for the status of this software.
+ * x2s.c - Convert XML to SML and back
+ *
+ * Test libxml2 SML parsing and saving ability.
+ * Run (x2s -?) to display a help screen.
+ *
+ * See Copyright for the status of this software.
+ *
+ * jf.larvoire@free.fr
  */
+
 #include <stdio.h>
 #include <string.h>
 #include <libxml/parser.h>
@@ -20,9 +18,6 @@
 
 #include <debugm.h> /* SysToolsLib debug macros */
 
-#define MY_ENCODING "ISO-8859-1"
-
-xmlChar *ConvertInput(const char *in, const char *encoding);
 int usage(void);
 
 int usage() {
@@ -105,7 +100,7 @@ int main(int argc, char *argv[]) {
       }
       if (!strcmp(opt, "s")) {
       	iSaveOpts |= XML_SAVE_AS_SML;
-	int iOutMLTypeSet = 1;
+	iOutMLTypeSet = 1;
       	continue;
       }
       if (!strcmp(opt, "S")) {
@@ -114,7 +109,7 @@ int main(int argc, char *argv[]) {
       }
       if (!strcmp(opt, "x")) {
       	iSaveOpts &= ~XML_SAVE_AS_SML;
-	int iOutMLTypeSet = 1;
+	iOutMLTypeSet = 1;
       	continue;
       }
       printf("Unexpected option: %s\n", arg);
@@ -136,12 +131,14 @@ int main(int argc, char *argv[]) {
   if (!infilename) infilename = "-"; /* Input from stdin by default */
   if (!outfilename) outfilename = "-"; /* Output to stdout by default */
 
+  /* Parse the input */
   doc = xmlReadFile(infilename, NULL, iParseOpts);
   if (doc == NULL) {
     fprintf(stderr, "Failed to parse ML in \"%s\"\n", infilename);
     return 1;
   }
   DEBUG_PRINTF(("# Parsed ML successfully\n"));
+
   /* Output the other ML type if not specified on the command line */
   if (doc->properties & XML_DOC_SML) {		/* The input doc was SML */
     DEBUG_PRINTF(("# The input was SML\n"));
@@ -150,6 +147,7 @@ int main(int argc, char *argv[]) {
     DEBUG_PRINTF(("# The input was XML\n"));
     if (!iOutMLTypeSet) iSaveOpts |= XML_SAVE_AS_SML;  /* So output SML */
   }
+
   /* Output an ?xml declaration only if there was one already */
   if (doc->properties & XML_DOC_XMLDECL) {	/* There was one */
     DEBUG_PRINTF(("# The input had an ?xml declaration\n"));
@@ -158,70 +156,14 @@ int main(int argc, char *argv[]) {
     DEBUG_PRINTF(("# The input did not have an ?xml declaration\n"));
     if (!iXmlDeclSet) iSaveOpts |= XML_SAVE_NO_DECL;
   }
+
+  /* Generate the output */
   /* xmlKeepBlanksDefault(1); // So that the SML is indented identically  */
   ctxt = xmlSaveToFilename(outfilename, NULL, iSaveOpts);
   xmlSaveDoc(ctxt, doc);
   xmlSaveClose(ctxt);
   xmlFreeDoc(doc);
   xmlCleanupParser(); /* Cleanup function for the XML library. */
+
   return 0;
-}
-
-/**
- * ConvertInput:
- * @in: string in a given encoding
- * @encoding: the encoding used
- *
- * Converts @in into UTF-8 for processing with libxml2 APIs
- *
- * Returns the converted UTF-8 string, or NULL in case of error.
- */
-xmlChar *
-ConvertInput(const char *in, const char *encoding)
-{
-    xmlChar *out;
-    int ret;
-    int size;
-    int out_size;
-    int temp;
-    xmlCharEncodingHandlerPtr handler;
-
-    if (in == 0)
-        return 0;
-
-    handler = xmlFindCharEncodingHandler(encoding);
-
-    if (!handler) {
-        printf("ConvertInput: no encoding handler found for '%s'\n",
-               encoding ? encoding : "");
-        return 0;
-    }
-
-    size = (int) strlen(in) + 1;
-    out_size = size * 2 - 1;
-    out = (unsigned char *) xmlMalloc((size_t) out_size);
-
-    if (out != 0) {
-        temp = size - 1;
-        ret = handler->input(out, &out_size, (const xmlChar *) in, &temp);
-        if ((ret < 0) || (temp - size + 1)) {
-            if (ret < 0) {
-                printf("ConvertInput: conversion wasn't successful.\n");
-            } else {
-                printf
-                    ("ConvertInput: conversion wasn't successful. converted: %i octets.\n",
-                     temp);
-            }
-
-            xmlFree(out);
-            out = 0;
-        } else {
-            out = (unsigned char *) xmlRealloc(out, out_size + 1);
-            out[out_size] = 0;  /*null terminating out */
-        }
-    } else {
-        printf("ConvertInput: no mem\n");
-    }
-
-    return out;
 }
