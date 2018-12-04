22f10305e78a484de860df44f56c8e418a3f767b
JFLarvoire <jf.larvoire@free.fr>
2018-12-04 15:04:39 +0100

    Merged in updates from MsvcLibX.

--- include/debugm.h
+++ include/debugm.h
@@ -127,6 +127,9 @@
 *		    Bug fix in _VSNPRINTF_EMULATION.			      *
 *    2017-10-30 JFL Added macro DEBUG_QUIET_LEAVE().			      *
 *    2018-02-02 JFL Added several missing DEBUG_xxx_COMMENT() macros.         *
+*    2018-04-25 JFL Added macro DEBUG_WPRINTF().                              *
+*                   Added macros DEBUG_WENTER() and DEBUG_WLEAVE().           *
+*    2018-10-01 JFL DEBUG_FREEUTF8() now clears the buffer pointer.           *
 *		    							      *
 *        (C) Copyright 2016 Hewlett Packard Enterprise Development LP         *
 * Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
@@ -177,7 +180,7 @@ int _vsnprintf(char *pBuf, int iBufSize, const char *pszFormat, va_list vl) {  \
 #define DEBUG_VERSION " Debug"
 
 #define DEBUG_GLOBAL_VARS \
-int iDebug0 = 0;		/* Global variable enabling debug output if TRUE. */   \
+static int iDebug0 = 0;	/* Global variable enabling debug output if TRUE. */   \
 int DEBUG_TLS iIndent = 0; /* Global variable controlling debug indentation. */\
 int (*pdputs)(const char *) = puts; /* Debug output routine. Default: puts */  \
 
@@ -227,7 +230,7 @@ int debug_printf(char *pszFormat, ...) {                                       \
 
 /* XMLPUBVAR int iDebug;	// Global variable enabling of disabling debug messages */
 XMLPUBFUN int * XMLCALL get_iDebug(void);
-#define iDebug *get_iDebug()
+#define iDebug (*get_iDebug())
 XMLPUBVAR int (*pdputs)(const char *);	/* Pointer to the debug puts routine */
 #if !defined(_MSDOS)
 XMLPUBFUN int  XMLCALL debug_printf(char *fmt,...);	/* Print debug messages */
@@ -256,6 +259,7 @@ extern DEBUG_TLS int iIndent;	/* Debug messages indentation. Thread local. */
 /* The enter and leave variants print, then respectively increase or decrease indentation,
    to make recursive calls easier to review. */
 #define DEBUG_FPRINTF(args) DEBUG_DO(if (DEBUG_IS_ON()) {DEBUG_PRINT_INDENT(); fprintf args;})
+#define DEBUG_WPRINTF(args) DEBUG_DO(if (DEBUG_IS_ON()) {DEBUG_PRINT_INDENT(); wprintf args;})
 #if defined(_MSDOS)
 #define DEBUG_PRINTF(args) DEBUG_DO(if (DEBUG_IS_ON()) {DEBUG_PRINT_INDENT(); printf args; fflush(stdout);})
 #define XDEBUG_PRINTF(args) DEBUG_DO(if (XDEBUG_IS_ON()) {DEBUG_PRINT_INDENT(); printf args; fflush(stdout);})
@@ -264,7 +268,9 @@ extern DEBUG_TLS int iIndent;	/* Debug messages indentation. Thread local. */
 #define XDEBUG_PRINTF(args) DEBUG_DO(if (XDEBUG_IS_ON()) {debug_printf args;})
 #endif /* defined(_MSDOS) */
 #define DEBUG_ENTER(args)  DEBUG_DO(DEBUG_PRINTF(args); iIndent += DEBUG_INDENT_STEP;)
+#define DEBUG_WENTER(args)  DEBUG_DO(DEBUG_WPRINTF(args); iIndent += DEBUG_INDENT_STEP;)
 #define DEBUG_LEAVE(args)  DEBUG_DO(DEBUG_PRINTF(args); iIndent -= DEBUG_INDENT_STEP;)
+#define DEBUG_WLEAVE(args)  DEBUG_DO(DEBUG_WPRINTF(args); iIndent -= DEBUG_INDENT_STEP;)
 #define DEBUG_QUIET_LEAVE()  DEBUG_DO(iIndent -= DEBUG_INDENT_STEP;)
 
 #define DEBUG_RETURN_INT(i, comment) DEBUG_DO(int DEBUG_i = (i); \
@@ -355,10 +361,13 @@ extern DEBUG_TLS int iIndent;	/* Debug messages indentation. Thread local. */
 #define DEBUG_PRINT_INDENT() DEBUG_DO_NOTHING() /* Print call-depth spaces */
 
 #define DEBUG_FPRINTF(args) DEBUG_DO_NOTHING()  /* Print a debug string to a stream */
+#define DEBUG_WPRINTF(args) DEBUG_DO_NOTHING()  /* Print a wide debug string to stdout */
 #define DEBUG_PRINTF(args)  DEBUG_DO_NOTHING()  /* Print a debug string to stdout */
 #define XDEBUG_PRINTF(args) DEBUG_DO_NOTHING()  /* Print an extra debug string to stdout */
 #define DEBUG_ENTER(args)   DEBUG_DO_NOTHING()  /* Print and increase indent */
+#define DEBUG_WENTER(args)  DEBUG_DO_NOTHING()  /* Print and increase indent */
 #define DEBUG_LEAVE(args)   DEBUG_DO_NOTHING()  /* Print and decrease indent */
+#define DEBUG_WLEAVE(args)  DEBUG_DO_NOTHING()  /* Print and decrease indent */
 #define DEBUG_QUIET_LEAVE() DEBUG_DO_NOTHING()  /* Print and decrease indent */
 
 #define DEBUG_RETURN_INT(i, comment) return(i)
@@ -426,7 +435,8 @@ extern DEBUG_TLS int iIndent;	/* Debug messages indentation. Thread local. */
     } while (0);				\
   ) /* DEBUG_FREE(pUtf8) MUST be used to free the UTF-8 string after use, else there will be a memory leak */
 
-#define DEBUG_FREEUTF8(pUtf8) DEBUG_CODE(free(pUtf8))
+/* Clear the pointer, so that DEBUG_FREEUTF8() can safely be called again */
+#define DEBUG_FREEUTF8(pUtf8) DEBUG_CODE(do {free(pUtf8); pUtf8 = NULL;} while (0))
 
 #endif /* defined(_WIN32) */
 
