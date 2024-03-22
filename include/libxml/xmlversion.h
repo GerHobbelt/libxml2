2abb52e8b74dd77071c31598f675ed35fe28c40c
Kelvin Lee <kiyolee@gmail.com>
2024-03-22 14:46:48 +1100

    Update following upstream, current version is 2.12.6

--- include/libxml/xmlversion.h
+++ include/libxml/xmlversion.h
@@ -15,21 +15,21 @@
  *
  * the version string like "1.2.3"
  */
-#define LIBXML_DOTTED_VERSION "2.12.5"
+#define LIBXML_DOTTED_VERSION "2.12.6"
 
 /**
  * LIBXML_VERSION:
  *
  * the version number: 1.2.3 value is 10203
  */
-#define LIBXML_VERSION 21205
+#define LIBXML_VERSION 21206
 
 /**
  * LIBXML_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "10203"
  */
-#define LIBXML_VERSION_STRING "21205"
+#define LIBXML_VERSION_STRING "21206"
 
 /**
  * LIBXML_VERSION_EXTRA:
@@ -44,7 +44,7 @@
  * Macro to check that the libxml version in use is compatible with
  * the version the software has been compiled against
  */
-#define LIBXML_TEST_VERSION xmlCheckVersion(21205);
+#define LIBXML_TEST_VERSION xmlCheckVersion(21206);
 
 /**
  * LIBXML_THREAD_ENABLED:
