feeb0dc5acfc36d9994a4f91e8e308325b65b7fe
Kelvin Lee <kiyolee@gmail.com>
2024-05-24 18:38:18 +1000

    Allow xmlMemoryDump() to be used in xsltproc

--- include/libxml/xmlmemory.h
+++ include/libxml/xmlmemory.h
@@ -158,7 +158,9 @@ XMLPUBFUN void
 XML_DEPRECATED
 XMLPUBFUN void
 	xmlMemShow	(FILE *fp, int nr);
+#if !defined(IN_XSLTPROC)
 XML_DEPRECATED
+#endif
 XMLPUBFUN void
 	xmlMemoryDump	(void);
 XMLPUBFUN void *
