d4f5d05b979c85c558c43e42598168499f4f1764
Ger Hobbelt <ger@hobbelt.com>
2024-11-01 03:21:14 +0100

    fix compiler warning about octal constant in string flowing into subsequent digit char.

--- xzlib.c
+++ xzlib.c
@@ -281,7 +281,7 @@ is_format_xz(xz_statep state)
 {
     lzma_stream *strm = &(state->strm);
 
-    return strm->avail_in >= 6 && memcmp(state->in, "\3757zXZ", 6) == 0;
+    return strm->avail_in >= 6 && memcmp(state->in, "\375" "7zXZ", 6) == 0;
 }
 
 static int
